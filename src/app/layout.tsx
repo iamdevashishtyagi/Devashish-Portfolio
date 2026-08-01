3bcfbe07dd4164860120ff63bc4502a8966c296e
Devashish Tyagi <iamdevashish@gmail.com>
2026-08-02 02:21:17 +0530

    Rag Widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, MedievalSharp  } from "next/font/google";
 import ScrollBackground from "./components/layout/ScrollBackground";
 import "./globals.css";
+import Script from 'next/script'
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://iamdevashishtyagi.vercel.app";
 
@@ -70,6 +71,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} ${medievalSharp.variable} h-full antialiased`}
     >
+      <Script
+        src="https://devsai-alpha.vercel.app/rag-chat-widget.js"
+        data-api-key={process.env.NEXT_PUBLIC_RAG_WIDGET_API_KEY}
+        data-api-url="https://rag-api-cb8d.onrender.com/api"
+        data-title="Portfolio assistant"
+        strategy="afterInteractive"
+      />
       <body className="min-h-full flex flex-col transition-colors duration-700">
         {/* <ScrollBackground /> */}
         {children}
